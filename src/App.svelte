db012c49b0008a963a3f9066977db5b0239940e0
Carlos Benavides <1992cab@gmail.com>
2020-09-15 19:36:02 -0500

    chore: added shopping modal

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,10 @@
   import Navbar from "./components/Navbar.svelte";
   import Products from "./components/Products.svelte";
   import Footer from "./components/Footer.svelte";
+  import ShoppingModal from "./components/ShoppingModal.svelte";
   export let name: string;
+
+  let showModal = false;
 </script>
 
 <style>
@@ -25,6 +28,17 @@
       max-width: none;
     }
   }
+
+  .fab-modal {
+    position: fixed;
+    right: 0;
+    padding-right: 10px;
+    text-align: left;
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+    box-shadow: 2px 2px 4px rgb(0, 0, 0, 0.4);
+    bottom: 320px;
+  }
 </style>
 
 <Navbar />
@@ -32,5 +46,12 @@
   <h1>Hello {name}!</h1>
   <p>Nuestros pasteles únicos a tu alcance</p>
 </main>
+<button class="fab-modal" on:click={() => (showModal = true)}>
+  <img src="./icons/shopping-cart.svg" alt="shopping-cart.svg" width="25" />
+</button>
 <Products />
 <Footer />
+
+{#if showModal}
+  <ShoppingModal on:close={() => (showModal = false)} />
+{/if}
---
+++ src/components/ShoppingModal.svelte
@@ -0,0 +1,61 @@
+<script>
+  import { createEventDispatcher, onDestroy } from "svelte";
+
+  const dispatch = createEventDispatcher();
+  const close = () => dispatch("close");
+</script>
+
+<style>
+  .shopping-modal {
+    border-top-left-radius: 8px;
+    box-shadow: -3px -3px 4px rgb(0, 0, 0, 0.2);
+    width: 280px;
+    min-height: 320px;
+    padding: 1.5em;
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    background-color: white;
+  }
+
+  .shopping-modal h4,
+  .shopping-modal p {
+    margin: 0 0 1em 0;
+  }
+
+  .shopping-modal p {
+    font-size: 14px;
+  }
+
+  .close-button {
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: 1.5em;
+    cursor: pointer;
+  }
+</style>
+
+<div class="shopping-modal">
+  <span class="close-button" on:click={close}><img
+      src="icons/times.svg"
+      alt="times.svg"
+      width="20" />
+  </span>
+  <h4>
+    <img class="icon s2" src="./icons/factura.svg" alt="factura.svg" />
+    <span>Mi pedido</span>
+  </h4>
+  <p>Nombre: Carlos Benavides</p>
+  <p>Dirección: Carrera 28 # 5 -13</p>
+  <p>Barrio: Las Orquideas</p>
+  <p>2 tortas de durazno pequeñas - $ 20.000</p>
+  <p>1 torta de Chocolate mediana - $ 24.000</p>
+  <p>Total pedido $44.000</p>
+  <p>Voy a pagar con: $50.000</p>
+  <button class="primary">
+    <img class="icon" src="./icons/whatsapp.svg" alt="whatsapp.svg" />
+    <span>Enviar</span>
+  </button>
+  <!-- <button on:click={close}>Vaciar</button> -->
+</div>
